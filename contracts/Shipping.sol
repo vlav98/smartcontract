0266f689c1d30cdcaf7d6b426e563be733d398cb
Valentine <valentinelav98@gmail.com>
2021-04-22 16:40:09 +0200

    Shipping contract

--- contracts/Shipping.sol
+++ contracts/Shipping.sol
@@ -1,21 +1,42 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.21 <0.9.0;
 
 contract Shipping {
 
-    enum State {Pending, Shipped, Delivered}
-    
+    enum State {
+        Pending, 
+        Shipped, 
+        Delivered
+    }
+
     State public state;
+    address payable internal deliveryAccount = 0xc71fDbDE4938D7605528FD998a7a5F5420eAbB6A;
+    
+    event Create(uint id, State); 
+    event Shipped(uint id, State); 
+    event Delivered(uint id, State); 
 
-    function setPending() {
-        state = Pending;
+    struct Article {
+        uint _id;
+        State state;
     }
 
-    function setShipped() {
-        state = Shipped;
+    Article[] public articles;
+
+    function create(uint _id) public {        
+        articles.push(Article(_id, State.Pending));
+        emit Create(_id, State.Pending);
     }
 
-    function setDelivered() {
-        state = Delivered;
+    function shipped(uint _id) public {
+        emit Create(_id, State.Shipped);
+    }
+
+    function received(uint _id) public {
+        emit Delivered(_id, State.Delivered);
+    }
+
+    function giveTip(address deliveryAccount) public {
+
     }
 }
